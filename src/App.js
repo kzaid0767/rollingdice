19a333f9a7811022f8a323d1a79cb8badf4c0358
Kassim Zaid <kzaid0767@gmail.com>
2022-07-30 10:03:08 -0400

    work done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import MyDie from './MyDie';
 import RollDice from './RollDie';
+import RollingD from './RollingD';
 
 function App() {
   return (
     <div className="App">
-      <h2>Roll The Dice And Wath'em Dance</h2>
-      <RollDice/>
+      <h2>Roll The Dice And Watch'em Dance</h2>
+      <RollDice />
     </div>
   );
 }
